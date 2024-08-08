53c608f8a97520372a99f22e322dbf11ae776cce
ankkitsharma <ankitsharma2801@gmail.com>
2024-08-08 11:10:19 +0530

    setup dbConfig

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,6 +1,8 @@
 // Import the express in typescript file
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import pool from "../dbConfig";
 
 // Initialize the dotenv
 dotenv.config();
@@ -8,8 +10,19 @@ dotenv.config();
 // Initialize the express engine
 const app: express.Application = express();
 
+// Enable cors middleware
+app.use(cors());
+
 // Take a port from .env for running server.
 const port: number = Number(process.env.PORT);
+
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Error connecting to PostgresSQL database:", err);
+  } else {
+    console.log("Connected to PostgreSQL database at:", res.rows[0].now);
+  }
+});
 
 // Handling '/' Request
 app.get("/", (req, res) => {
